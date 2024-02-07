1a76622bea0a84e285f4d23e25968fa65fa1ec79
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-02-07 09:27:15 +0000

    Add MDC

---
+++ spring-katas/spring-mdc/src/main/java/com/example/mdc/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.mdc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping
+    public String get(@RequestHeader("X-Correlation-Id") String correlationId) {
+        try {
+            MDC.put("correlation-id", correlationId);
+            log.info("Logging hello request");
+            return "Hello, world!";
+        } finally {
+            MDC.clear();
+        }
+    }
+}
---
+++ spring-katas/spring-mdc/src/main/java/com/example/mdc/SpringMdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMdcApplication.class, args);
+	}
+
+}
