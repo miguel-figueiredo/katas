ecb5f2764b11f182b52fae255a0256283132afb6
Miguel Figueiredo <pedro.figueiredo@kuehne-nagel.com>
2024-02-14 10:08:13 +0000

    Add name store

---
+++ spring-katas/spring-name-store/src/main/java/com/example/namestore/SpringNameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.namestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNameStoreApplication.class, args);
+	}
+
+}
---
+++ spring-katas/spring-name-store/src/test/java/com/example/namestore/SpringNameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.namestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
